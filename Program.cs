30fbe5eb2c67f63b0cc01f67b814c1883e167cca
juancaberrocal56977-jpg <juancaberrocal56977@gmail.com>
2026-04-25 14:54:18 -0600

    Permisos con loggin

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UTN.Winform.Electronicos.UI.Mantenimientos;
+using UTN.Winform.Electronicos.UI.Principal;
+using UTN.Winform.Electronicos.UI.Seguridad;
+
+namespace AppDispocitivosElectronicos
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            frmLogin ofrmLogin = new frmLogin();
+
+            ofrmLogin.ShowDialog();
+
+            if (ofrmLogin.DialogResult == DialogResult.OK)
+                Application.Run(new FrmElectronicos());
+        }
+    }
+}
